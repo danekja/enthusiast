f5ea63e3be4c1b8d0c5df60da04317e27c897cf8
Jakub Danek <danek.ja@gmail.com>
2015-08-27 21:57:38 +0200

    User entity representation.

---
+++ lib/common/model/user.dart
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2015. Jakub Danek
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ *  To view a copy of this license, visit
+ *   http://creativecommons.org/licenses/by-nc-sa/4.0/.
+ */
+
+library enthusiast.model.user;
+
+import 'package:redstone_mapper/mapper.dart';
+import 'package:enthusiast/types/types.dart';
+import 'package:redstone_mapper_mongo/metadata.dart';
+import 'package:enthusiast/auth/authentication.dart';
+
+///Entity representing application's user.
+class User extends Schema implements UserDetails {
+
+  ///Primary key
+  @Id()
+  String id;
+
+  ///Unique, primary email, serves as username.
+  @Field()
+  @Email()
+  @NotEmpty()
+  String username;
+
+  ///User's name
+  @Field()
+  @NotEmpty()
+  String name;
+
+  ///Represent's user's password.
+  ///
+  /// When retrieved from persistence, this should be its hashed value.
+  @Field()
+  @NotEmpty()
+  String password;
+
+  /// Set of user's roles.
+  @Field()
+  Set<String> roles;
+
+  /// Empty constructor
+  User();
+
+  /// Constructor initializing all fields.
+  User.full(this.id, this.username, this.name, this.password);
+
+  /// Constructor for initialization of User object from JSON document.
+  User.fromJson(Map map) : this.full(map["id"], map['email'], map['name'],map['password']);
+
+  /// Implementation of UserDetails' method.
+  Set<String> getRoles() {
+    return roles;
+  }
+
+  /// Implementation of UserDetails' method.
+  String getUserDescription() {
+    return name;
+  }
+
+
+}
